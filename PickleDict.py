5303228f7ba519f299eb197c58f7ddba51a667c6
markansn <manson10@hotmail.co.uk>
2020-01-29 14:08:05 +0000

    pickling functionality now fully implemented in PickleDict

--- PickleDict.py
+++ PickleDict.py
@@ -1,17 +1,73 @@
 import pickle
 import os
-def convertToPickleFromDict(dict, filename):
-    with open(filename + '.pickle', 'wb') as handle:
-        pickle.dump(dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+import datetime
+def createPickle(dictionary, directory, filename):
+    MAX_DICT_NUMBER = 5
+    path = os.path.join(directory, filename + "." + "pickle")
+    print(path)
+    dict_list = []
+    if os.path.exists(path):
+        currentList = getPickle(directory, filename)
+
+        if len(currentList) == MAX_DICT_NUMBER:
+            currentList.pop(0)
+
+        currentList.append(dictionary)
+        dict_list = currentList
+
+    else:
+        dict_list = [dictionary]
+
+    with open(path, 'wb') as handle:
+        pickle.dump(dict_list, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
-def convertToDictFromPickle(filename):
-    with open(filename + '.pickle', 'rb') as handle:
+def getPickle(directory, filename):
+    path = os.path.join(directory, filename + "." + "pickle")
+    with open(path, 'rb') as handle:
         p = pickle.load(handle)
 
-    os.remove(filename + '.pickle')
+
     return p
 
+
+
+
+
+
+"""
+
+USE THESE THREE FUNCTIONS 
+
+"""
+# Fetch pickle
+
+
+def fetch(directory, filename):
+
+    return getPickle(directory, filename)[-1]
+
+
+# Store pickle
+
+
+def store(dictionary, directory, filename):
+
+    createPickle(dictionary, directory, filename)
+
+
+# Delete most recent pickle entry
+
+
+def revert(directory, filename):
+
+    p = getPickle(directory, filename)
+    if len(p) > 1:
+        p.pop(-1)
+    path = os.path.join(directory, filename + "." + "pickle")
+    os.remove(path)
+    with open(path, 'wb') as handle:
+        pickle.dump(p, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 
