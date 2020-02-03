c16fd8661aec7bd0fa64431c22aaf3d35584f1b6
Yansong Liu <38743168+LiuYYSS@users.noreply.github.com>
2020-02-03 16:48:25 +0000

    env

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\liu87\Desktop\UN project\Python 3.7 code src\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
